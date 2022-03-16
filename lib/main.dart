e86d049f65136ae81aac6ba627727e97571efdb8
markiplagat <markkiplagatkipruttoh@gmail.com>
2022-03-16 22:08:23 +0300

    make icons tappable

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,15 @@ class _RandomWordsState extends State<RandomWords> {
         color: alreadySaved ? Colors.red : null,
         semanticLabel: alreadySaved ? 'Remove from favorites' : "Save",
       ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 
